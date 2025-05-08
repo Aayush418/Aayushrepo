ae46f5fbd9e3d9db3082412f972bb1aba529ae85
Aayush418 <royalaayush32@gmail.com>
2025-05-08 19:10:54 +0545

    Add files via upload

---
+++ real interation.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int age=20, roll_no=12;
+char name[20]="Aayush Tamang";
+char gender='M';
+
+printf("All details");
+printf("AGE: %d\n", age );
+printf("ROLL NO: %d\n", roll_no );
+printf("NAME: %s\n", name);
+printf("GENDER: %c\n", gender);
+return 0;
+}
+
